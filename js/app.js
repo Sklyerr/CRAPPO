b975d861901c2a1cda1aebe1300ecf17fc52d252
Sklyerr <161568287+Sklyerr@users.noreply.github.com>
2024-11-20 09:43:13 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,27 @@
-// Функция-обработчик клика
-function handleClick(event) {
-  event.preventDefault(); // Предотвращаем переход по ссылке
-  const linkId = event.target.id; // Получаем id элемента
-  if (linkId == "menuProducts"){
-.   let block = document.getElementById('blockProducts');
-.   let blockPosition = block.getBoundingClientRect().top;
-.   let offsetPosition = blockPosition + window.scrollY
-
-.   window.scroll({
-.     top: offsetPosition,
-.     behavior: "smooth"
-})
+function blockGetId(blockId) {
+    let block = document.getElementById(blockId);
+    let blockPosition = block.getBoundingClientRect().top;
+    let offsetPosition = blockPosition + window.scrollY;
+    return offsetPosition; // Возвращаем значение offsetPosition
 }
 
+// Функция-обработчик клика
+function handleClick(event) {
+    event.preventDefault(); // Предотвращаем переход по ссылке
+    const linkId = event.target.id; // Получаем id элемента
+    if (linkId === "menuProducts") { // Сравнение строгое
+        let blockId = 'blockProducts';
+        let offsetPosition = blockGetId(blockId); // Получаем значение offsetPosition из функции
 
+        window.scroll({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
+    }
 }
 
 // Добавляем обработчик клика на все ссылки
 const links = document.querySelectorAll('a'); // Выбираем все теги <a>
 links.forEach(link => {
-  link.addEventListener('click', handleClick);
+    link.addEventListener('click', handleClick);
 });
